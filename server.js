a2dc6a31f73d9a6c7da256425463447c07b8d33e
stayasleep <virtuallyinfekted@gmail.com>
2017-10-03 17:45:01 -0700

    First! Simple node.js app demonstrating docker container ability!

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const PORT = 1337;
+const HOST = '0.0.0.0';
+const app = express();
+
+//client side requests will be directed here
+app.get('/', (req,res) => {
+	res.send("Hello Stayasleep\n");
+});
+
+app.listen(PORT,HOST, () => {
+	console.log(`server running on http://${HOST}:/${PORT}/`);
+});
